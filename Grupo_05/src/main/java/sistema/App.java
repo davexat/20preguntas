74470212ac6f9e683e6796c7b579d32678490241
davexat <yisuscristh20@gmail.com>
2024-08-19 17:28:03 -0500

    THE END 2

--- Grupo_05/src/main/java/sistema/App.java
+++ Grupo_05/src/main/java/sistema/App.java
@@ -1,14 +1,10 @@
 package sistema;
 
-import TDA.ArbolDecisionPRS;
-import clases.ArbolCreator;
 import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import javafx.scene.image.Image;
 
